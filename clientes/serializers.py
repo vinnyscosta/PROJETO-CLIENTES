03761eeec97e9d8843d477c6178c4f68ec81db7f
VINNYSCOSTA <44821757+vinnyscosta@users.noreply.github.com>
2024-01-04 17:01:10 -0300

    first commit

---
+++ clientes/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from clientes.models import Cliente
+
+class ClienteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cliente
+        fields = '__all__'
+    def validate_cpf(self, cpf):
+        if len(cpf) != 11:
+            raise serializers.ValidationError("O cpf deve ter 11 dígitos")
+        return cpf
+    
